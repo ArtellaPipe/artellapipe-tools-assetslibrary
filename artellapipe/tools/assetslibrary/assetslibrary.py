35a24cc352139633ee7fc7435d973dfd09e28486
Tomi Poveda <tpovedatd@gmail.com>
2019-11-19 02:39:11 +0100

    Added visual references for non published assets

--- artellapipe/tools/assetslibrary/assetslibrary.py
+++ artellapipe/tools/assetslibrary/assetslibrary.py
@@ -12,6 +12,7 @@ __license__ = "MIT"
 __maintainer__ = "Tomas Poveda"
 __email__ = "tpovedatd@gmail.com"
 
+import os
 import logging
 import traceback
 from functools import partial
@@ -22,10 +23,10 @@ from Qt.QtGui import *
 
 import tpDccLib as tp
 from tpQtLib.core import qtutils
-from tpQtLib.widgets import splitters
+from tpQtLib.widgets import splitters, stack, buttons
 
 import artellapipe
-from artellapipe.core import asset, tool
+from artellapipe.core import asset
 from artellapipe.utils import resource
 
 LOGGER = logging.getLogger()
@@ -41,7 +42,6 @@ class ArtellaAssetsLibraryWidget(QWidget, object):
         self.ui()
         self.resize(150, 800)
 
-        self._assets_viewer.update_assets()
         self._menu = self._create_contextual_menu()
 
     def ui(self):
@@ -58,6 +58,37 @@ class ArtellaAssetsLibraryWidget(QWidget, object):
         self._main_widget.setLayout(main_layout)
         self.main_layout.addWidget(self._main_widget)
 
+        self._stack = stack.SlidingStackedWidget()
+        main_layout.addWidget(self._stack)
+
+        no_assets_widget = QWidget()
+        no_assets_layout = QVBoxLayout()
+        no_assets_layout.setContentsMargins(2, 2, 2, 2)
+        no_assets_layout.setSpacing(2)
+        no_assets_widget.setLayout(no_assets_layout)
+        no_assets_frame = QFrame()
+        no_assets_frame.setFrameShape(QFrame.StyledPanel)
+        no_assets_frame.setFrameShadow(QFrame.Sunken)
+        no_assets_frame_layout = QHBoxLayout()
+        no_assets_frame_layout.setContentsMargins(2, 2, 2, 2)
+        no_assets_frame_layout.setSpacing(2)
+        no_assets_frame.setLayout(no_assets_frame_layout)
+        no_assets_layout.addWidget(no_assets_frame)
+        no_assets_found_label = QLabel()
+        no_assets_found_pixmap = resource.ResourceManager().pixmap('no_assets_found')
+        no_assets_found_label.setPixmap(no_assets_found_pixmap)
+        no_assets_frame_layout.addItem(QSpacerItem(10, 0, QSizePolicy.Expanding, QSizePolicy.Preferred))
+        no_assets_frame_layout.addWidget(no_assets_found_label)
+        no_assets_frame_layout.addItem(QSpacerItem(10, 0, QSizePolicy.Expanding, QSizePolicy.Preferred))
+        self._stack.addWidget(no_assets_widget)
+
+        viewer_widget = QWidget()
+        viewer_layout = QVBoxLayout()
+        viewer_layout.setContentsMargins(2, 2, 2, 2)
+        viewer_layout.setSpacing(2)
+        viewer_widget.setLayout(viewer_layout)
+        self._stack.addWidget(viewer_widget)
+
         self._assets_viewer = artellapipe.AssetsViewer(
             project=self._project,
             column_count=2,
@@ -69,7 +100,7 @@ class ArtellaAssetsLibraryWidget(QWidget, object):
         self._top_layout.setContentsMargins(0, 0, 0, 0)
         self._top_layout.setSpacing(2)
         self._top_layout.setAlignment(Qt.AlignCenter)
-        main_layout.addLayout(self._top_layout)
+        viewer_layout.addLayout(self._top_layout)
 
         self._categories_menu_layout = QHBoxLayout()
         self._categories_menu_layout.setContentsMargins(0, 0, 0, 0)
@@ -80,13 +111,13 @@ class ArtellaAssetsLibraryWidget(QWidget, object):
         self._categories_btn_grp = QButtonGroup(self)
         self._categories_btn_grp.setExclusive(True)
 
-        main_layout.addWidget(self._assets_viewer)
+        viewer_layout.addWidget(self._assets_viewer)
 
         self._supported_types_layout = QHBoxLayout()
         self._supported_types_layout.setContentsMargins(2, 2, 2, 2)
         self._supported_types_layout.setSpacing(2)
         self._supported_types_layout.setAlignment(Qt.AlignTop)
-        main_layout.addLayout(self._supported_types_layout)
+        viewer_layout.addLayout(self._supported_types_layout)
 
         self._supported_types_btn_grp = QButtonGroup(self)
         self._supported_types_btn_grp.setExclusive(True)
@@ -95,25 +126,61 @@ class ArtellaAssetsLibraryWidget(QWidget, object):
         self._sync_to_latest.setChecked(True)
         self._fit_camera_cbx = QCheckBox('Fit Camera')
         self._fit_camera_cbx.setChecked(True)
-        self.main_layout.addLayout(splitters.SplitterLayout())
+        viewer_layout.addLayout(splitters.SplitterLayout())
         checkboxes_layout = QHBoxLayout()
         checkboxes_layout.setContentsMargins(5, 5, 5, 5)
         checkboxes_layout.setSpacing(2)
-        self.main_layout.addLayout(checkboxes_layout)
+        viewer_layout.addLayout(checkboxes_layout)
         checkboxes_layout.addWidget(self._sync_to_latest)
         checkboxes_layout.addWidget(self._fit_camera_cbx)
         checkboxes_layout.addItem(QSpacerItem(10, 0, QSizePolicy.Expanding, QSizePolicy.Preferred))
-        self.main_layout.addLayout(splitters.SplitterLayout())
+        viewer_layout.addLayout(splitters.SplitterLayout())
 
         self._assets_viewer.assetAdded.connect(self._on_asset_added)
 
-        self.update_asset_categories()
-        self.update_supported_types()
+        self.refresh()
 
     def contextMenuEvent(self, event):
         if not self._menu:
             return
         self._menu.exec_(event.globalPos())
+
+    def refresh(self):
+        """
+        Function that refresh all the data of the assets library
+        """
+
+        self.update_asset_categories()
+        self.update_supported_types()
+        self.update_stack()
+        self.update_assets_status()
+
+    def update_stack(self):
+        """
+        Function that updates stack status taking into account current available assets count
+        """
+
+        total_assets = len(self._assets_viewer.get_assets())
+        if total_assets > 0:
+            self._stack.slide_in_index(1)
+        else:
+            self._stack.slide_in_index(0)
+
+    def update_assets_status(self):
+        """
+        Updates widgets icon depending of the availabilty of the asset
+        """
+
+        for i in range(self._assets_viewer.rowCount()):
+            for j in range(self._assets_viewer.columnCount()):
+                item = self._assets_viewer.cellWidget(i, j)
+                if not item:
+                    continue
+                asset_widget = item.containedWidget
+                asset_file = asset_widget.asset.get_file('rig', status=asset.ArtellaAssetFileStatus.PUBLISHED)
+                if asset_file and os.path.exists(asset_file):
+                    continue
+                self._create_sync_button(item)
 
     def update_asset_categories(self, asset_categories=None):
         """
@@ -187,6 +254,30 @@ class ArtellaAssetsLibraryWidget(QWidget, object):
 
         asset_widget.clicked.connect(self._on_asset_clicked)
 
+    def _create_sync_button(self, item):
+        """
+        Internal function that creates a sync button
+        :param item: ArtellaAssetWidget
+        """
+
+        sync_icon = resource.ResourceManager().icon('sync')
+        sync_hover_icon = resource.ResourceManager().icon('sync_hover')
+        sync_btn = buttons.HoverButton(icon=sync_icon, hover_icon=sync_hover_icon)
+        sync_btn.setStyleSheet('background-color: rgba(0, 0, 0, 150);')
+        sync_btn.setIconSize(QSize(50, 50))
+        sync_btn.move(item.width() * 0.5 - sync_btn.width() * 0.5, item.height() * 0.5 - sync_btn.height() * 0.5)
+        sync_btn.setParent(item.containedWidget)
+
+        not_published_pixmap = resource.ResourceManager().pixmap('asset_not_published')
+        not_published_lbl = QLabel()
+        not_published_lbl.move(9, 9)
+        not_published_lbl.setFixedSize(65, 65)
+        not_published_lbl.setPixmap(not_published_pixmap)
+        not_published_lbl.setParent(item.containedWidget)
+
+        asset_widget = item.containedWidget
+        sync_btn.clicked.connect(partial(self._on_sync_asset, asset_widget))
+
     def _create_contextual_menu(self):
         """
         Returns custom contextual menu
@@ -194,9 +285,10 @@ class ArtellaAssetsLibraryWidget(QWidget, object):
         """
 
         new_menu = QMenu(self)
-        get_thumbnails_action = QAction(
-            resource.ResourceManager().icon('picture'), 'Update Thumbnails', new_menu)
+        get_thumbnails_action = QAction(resource.ResourceManager().icon('picture'), 'Update Thumbnails', new_menu)
+        refresh_action = QAction(resource.ResourceManager().icon('refresh'), 'Refresh', new_menu)
         get_thumbnails_action.triggered.connect(self._on_update_thumbnails)
+        refresh_action.triggered.connect(self.refresh)
         new_menu.addAction(get_thumbnails_action)
 
         return new_menu
@@ -240,7 +332,8 @@ class ArtellaAssetsLibraryWidget(QWidget, object):
             if btn.isChecked():
                 try:
                     res = asset_widget.asset.reference_file_by_extension(
-                        extension=btn.extension, sync=self._sync_to_latest.isChecked())
+                        extension=btn.extension, file_type=btn.text().lower().replace(' ', ''),
+                        sync=self._sync_to_latest.isChecked())
                     if res:
                         if self._fit_camera_cbx.isChecked():
                             try:
@@ -255,13 +348,25 @@ class ArtellaAssetsLibraryWidget(QWidget, object):
                 finally:
                     return
 
+    def _on_sync_asset(self, asset_widget):
+        """
+        Internal callback function that is called when refresh button of an asset widget is pressed
+        """
 
-class ArtellaAssetsLibrary(tool.Tool, object):
+        if not asset_widget:
+            return
+
+        asset_widget.asset.sync_latest_published_files(None, True)
+        self.refresh()
+
+
+class ArtellaAssetsLibrary(artellapipe.Tool, object):
 
     LIBRARY_WIDGET = ArtellaAssetsLibraryWidget
 
     def __init__(self, project, config):
         super(ArtellaAssetsLibrary, self).__init__(project=project, config=config)
+
 
     def ui(self):
         super(ArtellaAssetsLibrary, self).ui()
@@ -269,3 +374,13 @@ class ArtellaAssetsLibrary(tool.Tool, object):
         supported_files = self.config.get('supported_files')
         self._library_widget = self.LIBRARY_WIDGET(project=self._project, supported_files=supported_files)
         self.main_layout.addWidget(self._library_widget)
+
+        artellapipe.Tracker().logged.connect(self._on_valid_login)
+
+    def _on_valid_login(self):
+        """
+        Internal callback function that is called anytime user log in into Tracking Manager
+        """
+
+        self._library_widget.refresh()
+
